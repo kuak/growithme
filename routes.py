1b24c7ca5325f595f847fa09ba8b55c726158f9e
José Manuel <josemanuel@kuak.me>
2013-05-11 12:13:42 -0600

    Recuperación de Login
    Se recuperó la pantalla de login que se perdió en las rutas con el
    commit anterior.

--- routes.py
+++ routes.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
