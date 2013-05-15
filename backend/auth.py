592dec62502e33218c3f0336e0a40369fad2b54b
unknown <honysu@gmail.com>
2013-05-14 22:41:34 -0600

    Agregado objeto para loguearse con fb

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,7 @@
 from flask_oauth import OAuth
 
-SECRET_TWITTER_KEY = ''
+FACEBOOK_APP_ID = '523024991088570'
+FACEBOOK_APP_SECRET = 'e75ff46a7b0bcd6b949db7fc14c7a1cf'
 
 oauth = OAuth()
 
@@ -11,4 +12,14 @@ twitter = oauth.remote_app('twitter',
     authorize_url='https://api.twitter.com/oauth/authenticate',
     consumer_key='F3MOphJZbUNFn7UdbNnwFg',
     consumer_secret='rVBDWIAYEJvehgwgA3fgDiKlXcANNr9shwOjLsi6nA'
+)
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
 )
